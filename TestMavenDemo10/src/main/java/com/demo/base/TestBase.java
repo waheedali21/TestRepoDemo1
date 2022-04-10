1e228d020b4b4102fe8f025d1b18098f26f01620
waheedali21 <waheedspc1@gmail.com>
2022-04-10 21:54:01 +0400

    This first commit

---
+++ TestMavenDemo10/src/main/java/com/demo/base/TestBase.java
@@ -0,0 +1,17 @@
+/**
+ * 
+ */
+package com.demo.base;
+
+/**
+ * @author h
+ *
+ */
+public class TestBase {
+	
+	
+	public void loadConfig() {
+		System.out.println("loading the prop file");
+	}
+
+}
---
+++ TestMavenDemo10/src/test/java/com/demo/testcases/Login.java
@@ -0,0 +1,17 @@
+/**
+ * 
+ */
+package com.demo.testcases;
+
+/**
+ * @author h
+ *
+ */
+public class Login {
+	
+	
+	public void doLogin() {
+		System.out.println("Login verification");
+	}
+
+}
